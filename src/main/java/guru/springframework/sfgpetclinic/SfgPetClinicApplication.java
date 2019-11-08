ce7cb0ab7b0ad9235f75e9a1efeeaaaa0f2daed6
MilanKp03 <40432786+MilanKp03@users.noreply.github.com>
2019-11-08 11:21:26 +0100

    test

--- src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -8,6 +8,11 @@ public class SfgPetClinicApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SfgPetClinicApplication.class, args);
+
+        System.out.println("test");
+
     }
+
+
 
 }
